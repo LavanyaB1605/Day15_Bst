c97ce2f241a5cd363b0ae1289f11318df5672da5
LavanyaB1605 <lavanya.bhaskaran16@gmail.com>
2022-10-27 17:45:32 +0530

    Added values

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,13 @@ public class Main extends BST_Class {
         bst.insert(56);
         bst.insert(30);
         bst.insert(70);
+        bst.insert(22);
+        bst.insert(11);
+        bst.insert(40);
+        bst.insert(70);
+        bst.insert(60);
+        bst.insert(65);
+        bst.insert(95);
         //print the BST
         System.out.println("The BST Created with input data(Left-root-right):");
         bst.inorder();
